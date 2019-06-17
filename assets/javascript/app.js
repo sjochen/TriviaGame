44e7b0d7405213fd553edb1fbc3f5f851abd0b20
Stephen Jochen <stephenjochen2@gmail.com>
2019-06-17 18:59:45 -0400

    updated the js so the questions and the answers appear on the screen and you can only select one answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,146 +1,172 @@
-var container = $('#trivia');
-var ansContainer = $('#answers');
-var resultsButton = $('#submit');
+
+
+
+var score = 0;
 
 
 
 
-//$('#submit').on('click' , myAnswers);
 
 
-const questions = [{
+var questions = [{
         question: "Who is the Regional Manager of Dunder Mifflin in season one?",
 
-        answers: {
-            a: "Michael Scott",
-            b: "Jim Halpert",
-            c: "Dwight Shrut",
-            d: "David Wallace"
-      },
+        answers: [
+             "Michael Scott",
+             "Jim Halpert",
+             "Dwight Shrut",
+             "David Wallace"
+        ],
 
         rightAnswer: "a"
     },
     {
         question: "How many minutes did Michael work for Dunder Mifflin?",
         
-        answers: {
-            a: "5,624,100 min",
-            b: "9,986,000 min",
-            c: "11,975,245 min",
-            d: "8,774,268 min"
-        },
+        answers: [
+             "5,624,100 min",
+             "9,986,000 min",
+             "11,975,245 min",
+             "8,774,268 min"
+        ],
         rightAnswer: "b"
     },
 
     {
         question: "What is Holly's middle name?",
 
-        answers: {
-            a: "Morgan",
-            b: "Tina",
-            c: "Molly",
-            d: "Partridge"
-        },
+        answers: [
+             "Morgan",
+             "Tina",
+             "Molly",
+             "Partridge"
+        ],
 
         rightAnswer: "d"
     },
     {
         question: "Who is the only character to appear in every episode",
 
-        answers: {
-            a: "Pam",
-            b: "Michael",
-            c: "Kelly",
-            d: "Dwight"
-        },
+        answers: [
+             "Pam",
+             "Michael",
+             "Kelly",
+             "Dwight"
+        ],
 
         rightAnswer: "d"
     },
     {
         question: "How many seasons of The Office are there?",
 
-        answers: {
-            a: "10",
-            b: "9",
-            c: "8",
-            d: "7"
-        },
+        answers: [
+             "10",
+             "9",
+             "8",
+             "7"
+        ],
 
         rightAnswer: "b"
     },
     {
         question: "How many boxes of cookies did Kevin say he wanted to buy?",
 
-        answers: {
-            a: "50",
-            b: "40",
-            c: "triple digits",
-            d: "he doesn't say"
-        },
+        answers: [
+             "50",
+             "40",
+             "triple digits",
+             "he doesn't say"
+        ],
 
         rightAnswer: "c"
     },
     {
         question: "What is the firt prank Jim pulls on Dwight?",
 
-        answers: {
-            a: "He puts Dwight's stapler in jell-o",
-            b: "He gift wraps Dwight's desk",
-            c: "He moves Dwight's desk to the bathroom",
-            d: "He sends Dwight on a secret FBI mission",
-    },
+        answers: [
+             "He puts Dwight's stapler in jell-o",
+             "He gift wraps Dwight's desk",
+             "He moves Dwight's desk to the bathroom",
+             "He sends Dwight on a secret FBI mission",
+        ],
 
         rightAnswer: "a"
     },
     {
         question: "What was Andy's nickname at Cornell?",
 
-        answers: {
-            a: "Nard Dog",
-            b: "B-Dog",
-            c: "Boner Champ",
-            d: "He didn't have a nickname"
-        },
+        answers: [
+             "Nard Dog",
+             "B-Dog",
+             "Boner Champ",
+             "He didn't have a nickname"
+        ],
 
         rightAnswer: "c"
     },
     {
         question: "What disease is Michael trying to raise awareness for with the fun run?",
 
-        answers: {
-            a: "AIDS",
-            b: "Measles",
-            c: "Herpes",
-            d: "Rabies"
-        },
+        answers: [
+             "AIDS",
+             "Measles",
+             "Herpes",
+             "Rabies"
+        ],
 
         rightAnswer: "d"
     },
     {
         question: "What is the name of Angela's cat that she throws through the ceiling?",
 
-        answers: {
-            a: "Bandit",
-            b: "Garbage",
-            c: "Sprikles",
-            d: "Mr. Whiskers"
-        },
+        answers: [
+             "Bandit",
+             "Garbage",
+             "Sprikles",
+             "Mr. Whiskers"
+        ],
 
         rightAnswer: "a"
     },
     
 ];
+gameStart();
+console.log(questions);
+function gameStart (){
+for(var i = 0; i < questions.length; i++) {
+    
+    var divMaker = $('<div class="quesHold">');
+    
+    var pMaker = $('<p>').text(questions[i].question);
+    
+    divMaker.append(pMaker);
+    
+    $('#trivia').append(divMaker);
+    
+    var ans = $('<ol type="a">');
+    
+    for(var j = 0; j < 4; j++){
+     var li = $('<li>');
+     var label = $('<label>');
+     var input = $('<input type="radio">').attr({name:'gues' + i, value: j});
 
-
-
-function displayQuestions() {
-    $('#trivia').text(questions);
+     label.text(questions[i].answers[j]);
+     label.prepend(input);
+     li.append(label);
+     ans.append(li);
+        
+    }
+    divMaker.append(ans);
+    }
 }
 
 
-$(document).ready(function () {
-    displayQuestions();
-});
+
+
+
+
+
+
 
 
 
